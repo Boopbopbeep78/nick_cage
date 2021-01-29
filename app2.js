5ea05d48f9a387d1751f2b1284499e332775eb15
Jeremy Brazell <jwbrazell@gmail.com>
2021-01-28 16:52:42 -0800

    adding file

--- app2.js
+++ app2.js
@@ -10,3 +10,42 @@ else if (userName != 'Yes') {
 }
 }
 getGreeting();
+
+
+function guessingGame(){
+    // Got random code from w3schools
+    let numberOfGuesses = 6
+    let correctAnswer = 111
+    console.log(correctAnswer);
+    let userAnswer = askUserQuestion();
+    for(let i = 0; i < numberOfGuesses; i++){
+        if (userAnswer == correctAnswer){
+            alert('You are Right!');
+            break;
+        } else if (userAnswer < correctAnswer){
+            alert('More Than That.  Try again.')
+        } else if (userAnswer > correctAnswer){
+            alert('Not That Many. Try again.')
+        }
+        userAnswer = askUserQuestion();
+    }
+    if(userAnswer != correctAnswer){
+        alert('Thank you for playing')
+    }
+}
+
+function askUserQuestion(){
+    let userAnswer = -1
+    while(userAnswer < 1 || userAnswer > 100){
+        userAnswer = prompt('How many movies has Nick Cage been in?');
+    }
+    return userAnswer;
+}
+
+        function imagesOnScreen(){
+    userAnswer = prompt('How many Nick Cages is enough?');
+    
+    for (let i =0; i < userAnswer;i++){
+            document.write('<img src=https://www.placecage.com/g/200/300>')
+            }
+    }
